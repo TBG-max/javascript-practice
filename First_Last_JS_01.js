2b8f4cf1de77c2392c1c70f39ffaafd04f371e94
TBG-max <cristian.perez@stu.vertexacademies.org>
2024-10-15 14:01:31 -0400

    Add files via upload

---
+++ First_Last_JS_01.js
@@ -0,0 +1,9 @@
+// This is a comment.  The // tells the computer to ignore this line.
+// Developers use comments to leave helpful notes to themselves and others.
+
+// Add your code below and click the 'Run code' button to see if it works!
+
+console.log("Hello, world!");
+console.log("¡hola, mundo!");
+console.log("Hello,\nWorld");
+console.log("The computor says, \"hello world\"");
