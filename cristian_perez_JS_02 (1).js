95f229d6aaa5510a1aa956ed8e0c19107b41c5d4
TBG-max <cristian.perez@stu.vertexacademies.org>
2024-10-16 14:08:35 -0400

    Add files via upload

---
+++ cristian_perez_JS_02 (1).js
@@ -0,0 +1,15 @@
+// Example:             // Expected Output
+console.log(4 + 2.5)   // 6.5
+
+// Write 5 more expressions below:
+console.log(4 * 7)      // 28
+console.log(37 + 10)    // 47
+console.log(21 % 9)     // 3
+console.log(7 ** 4)     // 2401
+console.log(63 / 9)     // 7
+console.log(!true);     // false
+console.log(!!false);   // false
+console.log(true || true);   // true
+console.log(false || true); // true
+console.log(true || true);  // true
+console.log(false || true); // true
