ab52f6203083ec462866713268fb5c41e2170a5e
TBG-max <cristian.perez@stu.vertexacademies.org>
2024-10-18 13:52:47 -0400

    Add files via upload

---
+++ Cristian_Perez_JS_03.js
@@ -0,0 +1,40 @@
+// 1. Declare a variable called `firstName` and assign it your first name as a string:
+
+
+// 2. Declare a variable called `lastName` and assign it your last name as a string:
+
+
+// 3. Declare a variable called `age`:
+
+
+// 4. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+
+
+// 5. Assign the `age` variable to a number:
+
+
+// 6. Print out the `firstName`, `lastName`, and `age` variables. What do you
+//    expect them to be when they get printed?
+
+
+let Fname = "cristian"
+let Lname = "perez";
+console.log("Full Name:", Fname +" "+ Lname);
+
+console.log("age:", 16)
+console.log("Birth Year", 2008)
+
+console.log("Vertex" == "vertex");
+console.log("10" + 15);
+console.log("Taco" * 2);
+
+let foo = "Hello";
+
+let bar = "World";
+
+console.log(foo + bar);
+
+bar = 100;
+
+console.log(foo + bar);
